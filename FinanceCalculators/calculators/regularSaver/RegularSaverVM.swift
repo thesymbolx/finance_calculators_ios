8aa374b2beb38fe393cf0a59902a75f3673a5fa4
Dale Evans <devans@ybs.co.uk>
2026-02-03 15:43:46 +0000

    updated calculation

--- FinanceCalculators/calculators/regularSaver/RegularSaverVM.swift
+++ FinanceCalculators/calculators/regularSaver/RegularSaverVM.swift
@@ -1,8 +1,20 @@
 import Combine
+import Foundation
 import SwiftUI
+
+extension Decimal {
+    func rounded(_ scale: Int, _ mode: NSDecimalNumber.RoundingMode) -> Decimal
+    {
+        var result = Decimal()
+        var source = self
+        NSDecimalRound(&result, &source, scale, mode)
+        return result
+    }
+}
 
 @Observable
 class RegularSaverVM: ObservableObject {
+
     struct AmountModel {
         var total: Decimal
         var graphPoints: [Point]
@@ -20,101 +32,94 @@ class RegularSaverVM: ObservableObject {
         var annualInterest: Decimal = 0
         var frequency: Frequency = .MONTHLY
     }
-    
+
     var calculatorInput = CalculatorInput()
-    private(set) var graphBalances: AmountModel = AmountModel(total: 0, graphPoints: [])
-    
+    private(set) var graphBalances: AmountModel = AmountModel(
+        total: 0,
+        graphPoints: []
+    )
+
     var isFormValid: Bool {
-            let isPrincipalValid = !calculatorInput.principal.isNaN
-            let isContributionValid = !calculatorInput.monthlyContribution.isNaN
-            let isInterestValid = !calculatorInput.annualInterest.isNaN
-            let isYearsValid = calculatorInput.noYears > 0
-            
-            return isPrincipalValid && isContributionValid && isInterestValid && isYearsValid
-        }
-    
-    func calculate() {
-        if(calculatorInput.frequency == .MONTHLY) {
-            compoundInterestMonthly()
-        } else if(calculatorInput.frequency == .ANNUALLY) {
-            compoundInterestYearly()
-        }
+        let isPrincipalValid = !calculatorInput.principal.isNaN
+        let isContributionValid = !calculatorInput.monthlyContribution.isNaN
+        let isInterestValid = !calculatorInput.annualInterest.isNaN
+        let isYearsValid = calculatorInput.noYears > 0
+
+        return isPrincipalValid && isContributionValid && isInterestValid
+            && isYearsValid
     }
 
-    private func compoundInterestMonthly() {
-        let noYears = calculatorInput.noYears
-        let principal = calculatorInput.principal
-        let annualInterest = calculatorInput.annualInterest
-        let monthlyContribution = calculatorInput.monthlyContribution
-        var balancePerYear: [[Decimal]] = []
-        var daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        let dailyInterestRate = (annualInterest / 100) / 365
-        var total = principal
+    func calculate() {
+        let balancePerYear = calculateBalance()
+        let endingBalance = balancePerYear.last!.last!
 
-        for year in 1...noYears {
-            var balancePerMonth: [Decimal] = []
-
-            if isLeapYear(year) {
-                daysInMonths[1] = 29
-            } else {
-                daysInMonths[1] = 28
-            }
-            
-            daysInMonths.forEach { day in
-                total += monthlyContribution
-                let accuredInterest = (total * dailyInterestRate) * Decimal(day)
-                balancePerMonth.append(total + accuredInterest)
-            }
-
-            balancePerYear.append(balancePerMonth)
-        }
-        
         graphBalances = AmountModel(
-            total: total,
+            total: endingBalance,
             graphPoints: toPoint(balancePerYear: balancePerYear)
         )
     }
-    
-    private func compoundInterestYearly() {
+
+    private func calculateBalance() -> [[Decimal]] {
+        let compoundFrequency = calculatorInput.frequency
         let noYears = calculatorInput.noYears
         let principal = calculatorInput.principal
         let annualInterest = calculatorInput.annualInterest
         let monthlyContribution = calculatorInput.monthlyContribution
+
         var balancePerYear: [[Decimal]] = []
         var daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
         let dailyInterestRate = (annualInterest / 100) / 365
         var total = principal
-        var accuredInterest: Decimal = 0
+        var accruedInterest: Decimal = 0
 
-        for year in 1...noYears {
+        for year in 0..<noYears {
             var balancePerMonth: [Decimal] = []
 
-            if isLeapYear(year) {
-                daysInMonths[1] = 29
-            } else {
-                daysInMonths[1] = 28
-            }
+            daysInMonths[1] = getDaysInFeb(currentYear: year)
 
-            daysInMonths.forEach { day in
+            for (monthIndex, daysInMonth) in daysInMonths.enumerated() {
+
                 total += monthlyContribution
-                accuredInterest += (total * dailyInterestRate) * Decimal(day)
+
+                let interestForMonth =
+                    (total * dailyInterestRate) * Decimal(daysInMonth)
+                accruedInterest += interestForMonth
+
+                switch compoundFrequency {
+                    case .MONTHLY:
+                        total += accruedInterest.rounded(2, .plain)
+                        accruedInterest = 0
+
+                    case .ANNUALLY:
+                        if monthIndex == 11 {
+                            total += accruedInterest.rounded(2, .plain)
+                            accruedInterest = 0
+                        }
+                }
+
                 balancePerMonth.append(total)
             }
-            
-            balancePerMonth[balancePerMonth.count - 1] += accuredInterest
 
             balancePerYear.append(balancePerMonth)
         }
-        
-        graphBalances = AmountModel(
-            total: total,
-            graphPoints: toPoint(balancePerYear: balancePerYear)
-        )
+
+        return balancePerYear
     }
 
-    private func isLeapYear(_ year: Int) -> Bool {
-        return year.isMultiple(of: 4)
-            && (!year.isMultiple(of: 100) || year.isMultiple(of: 400))
+    private func getDaysInFeb(currentYear: Int) -> Int {
+        let currentYear =
+            Calendar
+            .current
+            .component(.year, from: Date())
+            .advanced(by: currentYear)
+
+        let isLeapYear =
+            currentYear.isMultiple(of: 4)
+            && (!currentYear.isMultiple(of: 100)
+                || currentYear.isMultiple(of: 400))
+
+        return isLeapYear ? 29 : 28
     }
 
     private func toPoint(balancePerYear: [[Decimal]]) -> [Point] {
