80aa599e91c8c823682affb61a811c5b63c4a2e0
Dale Evans <devans@ybs.co.uk>
2026-02-09 12:55:26 +0000

    Updated text view

--- FinanceCalculators/ui/textViews/AmountInputView.swift
+++ FinanceCalculators/ui/textViews/AmountInputView.swift
@@ -5,37 +5,42 @@ struct AmountInputView: View {
     let label: String
     let prependSymbol: String
 
-
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
             Text(label)
                 .font(.system(size: 16, weight: .bold))
                 .foregroundColor(Color(white: 0.2))
 
-            HStack() {
+            HStack(spacing: 0) {
                 Text(prependSymbol)
                     .font(.system(size: 18, weight: .bold))
                     .frame(width: 45, height: 45)
-                    .foregroundColor(.white)
-                    .background(Color(.primary))
-                    .clipShape(
-                            UnevenRoundedRectangle(
-                                topLeadingRadius: 12,
-                                bottomLeadingRadius: 12,
-                                bottomTrailingRadius: 0,
-                                topTrailingRadius: 0
-                            )
-                        )
-                
+                    .foregroundColor(Color(.primary))
+                    .overlay(
+                        UnevenRoundedRectangle(
+                            topLeadingRadius: 12,
+                            bottomLeadingRadius: 12,
+                            bottomTrailingRadius: 0,
+                            topTrailingRadius: 0
+                        ).stroke(Color(.primary), lineWidth: 2)
+                    )
+
                 TextField("", value: $amount, format: .currency(code: ""))
                     .keyboardType(.decimalPad)
                     .font(.system(size: 16))
+                    .padding(.leading, 12)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 45)
+                    .overlay(
+                        UnevenRoundedRectangle(
+                            topLeadingRadius: 0,
+                            bottomLeadingRadius: 0,
+                            bottomTrailingRadius: 12,
+                            topTrailingRadius: 12
+                        ).stroke(Color.gray.opacity(0.5), lineWidth: 2)
+                    )
             }
             .background(Color.white)
-            .overlay(
-                RoundedRectangle(cornerRadius: 12)
-                    .stroke(Color.gray.opacity(0.5), lineWidth: 1)
-            )
         }
     }
 }
